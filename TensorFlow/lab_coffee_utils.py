f5a8585bc6efc64de2a81f7f3e000a2957b72ae9
Vipul Garg <vgarg1603@gmail.com>
2025-09-26 19:01:53 +0530

    Moved some files to this folder

---
+++ TensorFlow/lab_coffee_utils.py
@@ -0,0 +1,25 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.activations import sigmoid
+
+def load_coffee_data():
+    """ Creates a coffee roasting data set.
+        roasting duration: 12-15 minutes is best
+        temperature range: 175-260C is best
+    """
+    rng = np.random.default_rng(2)
+    X = rng.random(400).reshape(-1,2)
+    X[:,1] = X[:,1] * 4 + 11.5          # 12-15 min is best
+    X[:,0] = X[:,0] * (285-150) + 150  # 350-500 F (175-260 C) is best
+    Y = np.zeros(len(X))
+    
+    i=0
+    for t,d in X:
+        y = -3/(260-175)*t + 21
+        if (t > 175 and t < 260 and d > 12 and d < 15 and d<=y ):
+            Y[i] = 1
+        else:
+            Y[i] = 0
+        i += 1
+
+    return (X, Y.reshape(-1,1))
